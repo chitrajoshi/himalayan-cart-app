d42f355907f06edc41dad70013429df648c7e071
kamal pandey <kamal@MacBook-Air.local>
2026-06-29 19:06:28 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
@@ -10,6 +7,7 @@ function App() {
   return (
     <>
       <section id="center">
+        <h1>Himalayan Cart</h1>
         <button
           type="button"
           className="counter"
