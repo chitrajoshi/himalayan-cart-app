014b608bfbaca3c6cae9a0c6b84a11609d2d669d
kamal pandey <kamal@MacBook-Air.local>
2026-08-09 00:49:22 +0530

    graphql added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
 import { useState } from 'react'
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
 import './App.css'
 
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
 function App() {
-  const [count, setCount] = useState(0)
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+}
 
   return (
-    <>
-      <section id="center">
-        <h1>Himalayan Cart New branch</h1>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-    </>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+            <DisplayLocations />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, HttpLink, InMemoryCache, gql} from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
 import './index.css'
 import App from './App.jsx'
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://flyby-router-demo.herokuapp.com/',
+  }),
+  cache: new InMemoryCache(),
+});
+
+// const client = ...
+// client
+//   .query({
+//     query: gql`
+//       query GetLocations {
+//         locations {
+//           id
+//           name
+//           description
+//           photo
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
